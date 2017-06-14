2daac7a66cf4ac21529c9d02e4aa0f2301d4bca4
N-sight <lapskii.v@gmail.com>
2017-06-14 21:39:02 +0300

    исправляем доступ к SQL базе

--- helpers/const.php
+++ helpers/const.php
@@ -1,8 +1,8 @@
 <?php
     define ("server_name",'localhost');
-    define ("db_name",'hr_helper');
-    define ("username",'hr_help');
-    define ("pass_word",'76bbvwC3bxOachcF79QY');
+    define ("db_name",'admin_hr_helper');
+    define ("username",'admin_hr_help');
+    define ("pass_word",'f290e89ab3fb98bced277bc43aab8c0f');
 
     define ("path",'/hr/');
 
